066e8159e810326d1ac08c6e355edeee015eada9
bpresicci <80779795+bpresicci@users.noreply.github.com>
2021-03-24 12:19:39 +0100

    Add files via upload

--- plot_and_save.py
+++ plot_and_save.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 
-def plot_and_save(data_to_plot, data, i, n, ch_start, samples, fs, t, delta, save_pic, name_image=None):
+def plot_and_save(data_to_plot, data, n, ch_start, samples, fs, t, delta, save_pic, name_image='/content/none.png'):
+    for i in range(n):
         plt.plot(data_to_plot[i + ch_start, :] + delta * i * 2)
     plt.yticks(range(n) * delta * 2, data.ch_names[ch_start:ch_start + n])
     plt.xticks(range(0, samples + 1, fs), range(t + 1))
@@ -10,4 +11,5 @@ def plot_and_save(data_to_plot, data, i, n, ch_start, samples, fs, t, delta, sav
         image.savefig(name_image, dpi=200)
         image.clear()
         plt.close(image)
+    else:
         plt.show()
