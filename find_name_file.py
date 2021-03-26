f0719dd60ccf484401194a87d6979c16ac10a14c
bpresicci <80779795+bpresicci@users.noreply.github.com>
2021-03-26 10:53:40 +0100

    Add files via upload

--- find_name_file.py
+++ find_name_file.py
@@ -1,38 +1,32 @@
-def find_name_file(subject, save_pic, name_folder):
-    if save_pic and name_folder!='none':
+def find_name_file(subject, save_pic, name_folder, task):
+    if save_pic and name_folder != 'none':
         if subject < 10:
-            file = 'S00%sR01.edf' % subject
-            name_folder = '/content/%s' % name_folder
-            name_subject = '/subject00%s.png' % subject
-            name_image = name_folder + name_subject
+            file = 'S00%sR0%s.edf' % (subject, task)
+            name_image = "/content/%s/subject00%sR0%s" % (name_folder, subject, task)
         if subject > 9 and subject < 100:
-            file = 'S0%sR01.edf' % subject
-            name_folder = '/content/%s' % name_folder
-            name_subject = '/subject0%s.png' % subject
-            name_image = name_folder + name_subject
+            file = 'S0%sR0%s.edf' % (subject, task)
+            name_image = "/content/%s/subject0%sR0%s" % (name_folder, subject, task)
         if subject > 99:
-            file = 'S%sR01.edf' % subject
-            name_folder = '/content/%s' % name_folder
-            name_subject = '/subject%s.png' % subject
-            name_image = name_folder + name_subject
+            file = 'S%sR0%s.edf' % (subject, task)
+            name_image = "/content/%s/subject%sR0%s" % (name_folder, subject, task)
     else:
         if save_pic:
             if subject < 10:
-                file = 'S00%sR01.edf' % subject
-                name_image = '/content/subject00%s.png' % subject
+                file = 'S00%sR0%s.edf' % (subject, task)
+                name_image = '/content/subject00%sR0%s.png' % (subject, task)
             if subject > 9 and subject < 100:
-                file = 'S0%sR01.edf' % subject
-                name_image = '/content/subject0%s.png' % subject
+                file = 'S0%sR0%s.edf' % (subject, task)
+                name_image = '/content/subject0%sR0%s.png' % (subject, task)
             if subject > 99:
-                file = 'S%sR01.edf' % subject
-                name_image = '/content/subject%s.png' % subject
+                file = 'S%sR0%s.edf' % (subject, task)
+                name_image = '/content/subject%sR0%s.png' % (subject, task)
         else:
             if subject < 10:
-                file = 'S00%sR01.edf' % subject
+                file = 'S00%sR0%s.edf' % (subject, task)
             if subject > 9 and subject < 100:
-                file = 'S0%sR01.edf' % subject
+                file = 'S0%sR0%s.edf' % (subject, task)
             if subject > 99:
-                file = 'S%sR01.edf' % subject
+                file = 'S%sR0%s.edf' % (subject, task)
     if not save_pic:
         name_image = '/content/none.png'
     return file, name_image
