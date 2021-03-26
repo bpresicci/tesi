a844f9c9c56cb5ea51046384c2d6646ff73815af
bpresicci <80779795+bpresicci@users.noreply.github.com>
2021-03-26 11:56:07 +0100

    Add files via upload

--- find_name_file.py
+++ find_name_file.py
@@ -1,32 +1,8 @@
-def find_name_file(subject, save_pic, name_folder, task):
-    if save_pic and name_folder != 'none':
-        if subject < 10:
-            file = 'S00%sR0%s.edf' % (subject, task)
-            name_image = "/content/%s/subject00%sR0%s" % (name_folder, subject, task)
-        if subject > 9 and subject < 100:
-            file = 'S0%sR0%s.edf' % (subject, task)
-            name_image = "/content/%s/subject0%sR0%s" % (name_folder, subject, task)
-        if subject > 99:
-            file = 'S%sR0%s.edf' % (subject, task)
-            name_image = "/content/%s/subject%sR0%s" % (name_folder, subject, task)
-    else:
-        if save_pic:
-            if subject < 10:
-                file = 'S00%sR0%s.edf' % (subject, task)
-                name_image = '/content/subject00%sR0%s.png' % (subject, task)
-            if subject > 9 and subject < 100:
-                file = 'S0%sR0%s.edf' % (subject, task)
-                name_image = '/content/subject0%sR0%s.png' % (subject, task)
-            if subject > 99:
-                file = 'S%sR0%s.edf' % (subject, task)
-                name_image = '/content/subject%sR0%s.png' % (subject, task)
-        else:
-            if subject < 10:
-                file = 'S00%sR0%s.edf' % (subject, task)
-            if subject > 9 and subject < 100:
-                file = 'S0%sR0%s.edf' % (subject, task)
-            if subject > 99:
-                file = 'S%sR0%s.edf' % (subject, task)
-    if not save_pic:
-        name_image = '/content/none.png'
-    return file, name_image
+def find_name_file(subject, task):
+    if subject < 10:
+        file = 'S00%sR0%s.edf' % (subject, task)
+    if subject > 9 and subject < 100:
+        file = 'S0%sR0%s.edf' % (subject, task)
+    if subject > 99:
+        file = 'S%sR0%s.edf' % (subject, task)
+    return file
