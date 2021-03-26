e49f5f090bec577b2bf92441b051cc94a3b1fd15
bpresicci <80779795+bpresicci@users.noreply.github.com>
2021-03-26 17:07:01 +0100

    Add files via upload

---
+++ check_subjects.py
@@ -0,0 +1,10 @@
+def check_subjects(subject_start, subject_end):
+    n_subjects = subject_end - subject_start
+    check_subjects = np.arange(subject_start, subject_end)
+    if 88 in check_subjects:
+        n_subject -= 1
+    if 92 in np.arange(subject_start, subject_end):
+        n_subject -= 1
+    if 100 in check_subjects:
+        n_subject -= 1
+    return n_subjects
