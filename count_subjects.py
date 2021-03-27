d25b4f039083a2f41ab8306c8e737490a69a09df
bpresicci <80779795+bpresicci@users.noreply.github.com>
2021-03-27 11:03:12 +0100

    Update and rename check_subjects.py to count_subjects.py

--- count_subjects.py
+++ count_subjects.py
@@ -1,4 +1,4 @@
-def check_subjects(subject_start, subject_end):
+def count_subjects(subject_start, subject_end):
     n_subjects = subject_end - subject_start
     check_subjects = np.arange(subject_start, subject_end)
     if 88 in check_subjects:
@@ -7,4 +7,4 @@ def check_subjects(subject_start, subject_end):
         n_subject -= 1
     if 100 in check_subjects:
         n_subject -= 1
-    return n_subjects
+    return n_subjects*2
