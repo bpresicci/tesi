46160f1a1e62cfbdc9795c417e268cf089c0a12b
bpresicci <80779795+bpresicci@users.noreply.github.com>
2021-03-27 19:23:37 +0100

    Update count_subjects.py

--- count_subjects.py
+++ count_subjects.py
@@ -6,12 +6,23 @@ def count_subjects(subject_start, subject_end):
   check_92 = 92 in check_subjects
   check_100 = 100 in check_subjects
   if  check_88 or check_92 or check_100:
-    if 88 in check_subjects:
-      n_subjects = subject_end - subject_start -1
-    if 92 in np.arange(subject_start, subject_end):
-      n_subjects = subject_end - subject_start -1
-    if 100 in check_subjects:
+    if check_88:
+      if check_92:
+        if check_100:
+          n_subjects = subject_end - subject_start - 3
+        else:
+          n_subjects = subject_end - subject_start - 2
+      else:
         n_subjects = subject_end - subject_start -1
+    else:
+      if check_92:
+        if check_100:
+          n_subjects = subject_end - subject_start - 2
+        else:
+          n_subjects = subject_end - subject_start -1
+      else:
+        if check_100:
+          n_subjects = subject_end - subject_start -1          
   else:
     n_subjects = subject_end - subject_start     
   return n_subjects*2
