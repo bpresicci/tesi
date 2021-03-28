99ebb24095b3a75418d0d14b087e66ada714d6e8
bpresicci <80779795+bpresicci@users.noreply.github.com>
2021-03-28 18:09:44 +0200

    Add files via upload

--- choose_epochs.py
+++ choose_epochs.py
@@ -1,18 +1,10 @@
-#voglio trovare un criterio per usare alcune epoche. Provo tagliando in base allo score
+#voglio trovare un criterio per usare alcune epoche.
 import numpy as np
-def min_score(idx_best, epoch, scores, min_percentage):
-    scores_mean = np.mean(scores)
-    min_scores = scores_mean * min_percentage
-    new_idx_best = []
-    for i in range(len(idx_best)):
-        if scores[i] >= min_scores:
-            new_idx_best = new_idx_best + [idx_best[i]]
-    new_idx_best = np.array(new_idx_best)
-    if len(new_idx_best) != len(idx_best):
-        epoch_list = np.zeros((len(new_idx_best), len(epoch[0]), len(epoch[0][0])))
-        n_epochs = len(new_idx_best)
-        for i in range(n_epochs):
-            epoch_list[i] = epoch[new_idx_best[i]]
-    else:
-        epoch_list = epoch
-    return new_idx_best, epoch_list
+def remove_epochs(idx_best, epoch, n_removed_epochs):
+  new_n_epoch = len(idx_best) - n_removed_epochs
+  new_idx_best = np.zeros(new_n_epoch)
+  new_epoch = np.zeros((new_n_epoch, len(epoch[0]), len(epoch[0][0])))
+  for i in range(new_n_epoch):
+    new_idx_best[i] = idx_best[i]
+    new_epoch[i] = epoch[idx_best[i]]
+    return new_idx_best, new_epoch
