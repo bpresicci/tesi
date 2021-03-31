f5297011f6187acd2bb3c2bcd7ef6d262e055d9d
bpresicci <80779795+bpresicci@users.noreply.github.com>
2021-03-31 18:39:36 +0200

    Update find_name_file_and_label.py

--- find_name_file_and_label.py
+++ find_name_file_and_label.py
@@ -1,12 +1,13 @@
 def find_name_file_and_label(subject, condition):
-    if condition == 1:
-        label = 0
-    if condition == 2:
-        label = 1
-    if subject < 10:
-        file = 'S00%sR0%s.edf' % (subject, condition)
-    if subject > 9 and subject < 100:
-        file = 'S0%sR0%s.edf' % (subject, condition)
-    if subject > 99:
-        file = 'S%sR0%s.edf' % (subject, condition)
-    return file, label
+  condition += 1 #condition assume valore 0 e 1 ma mi serve 1 e 2 per il dataset
+  if condition == 1:
+      label = 0
+  if condition == 2:
+      label = 1
+  if subject < 10:
+      file = 'S00%sR0%s.edf' % (subject, condition)
+  if subject > 9 and subject < 100:
+      file = 'S0%sR0%s.edf' % (subject, condition)
+  if subject > 99:
+      file = 'S%sR0%s.edf' % (subject, condition)
+  return file, label
