4e10833f263645842287af114f8abeb49f65ea69
bpresicci <80779795+bpresicci@users.noreply.github.com>
2021-03-31 18:28:36 +0200

    Add files via upload

--- download_edf.py
+++ download_edf.py
@@ -1,14 +1,15 @@
 import os
 
-def download_edf(subject_start, subject_end, task):
-    subject = subject_start
-    while subject < subject_end:
-        if subject < 10:
-            url = 'https://physionet.org/files/eegmmidb/1.0.0/S00%s/S00%sR0%s.edf' % (subject, subject, task)
-        if subject > 9 and subject < 100:
-            url = 'https://physionet.org/files/eegmmidb/1.0.0/S0%s/S0%sR0%s.edf' % (subject, subject, task)
-        if subject > 99:
-            url = 'https://physionet.org/files/eegmmidb/1.0.0/S%s/S%sR0%s.edf' % (subject, subject, task)
-        os.system('wget %s' % url)
-        subject += 1
+def download_edf(subject_start, subject_end, condition):
+    for subject in range(subject_start, subject_end):
+        if subject not in [88, 92, 100]:
+            if subject < 10:
+                url = 'https://physionet.org/files/eegmmidb/1.0.0/S00%s/S00%sR0%s.edf' % (subject, subject, condition)
+            if subject > 9 and subject < 100:
+                url = 'https://physionet.org/files/eegmmidb/1.0.0/S0%s/S0%sR0%s.edf' % (subject, subject, condition)
+            if subject > 99:
+                url = 'https://physionet.org/files/eegmmidb/1.0.0/S%s/S%sR0%s.edf' % (subject, subject, condition)
+            os.system('wget %s' % url)
+        else:
+            continue
 
