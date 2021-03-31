8593b24a90ffd23f70a0391d8cc0c4dd06959a3d
bpresicci <80779795+bpresicci@users.noreply.github.com>
2021-03-31 18:28:17 +0200

    Delete find_name_file.py

--- find_name_file.py
+++
@@ -1,8 +0,0 @@
-def find_name_file(subject, task):
-    if subject < 10:
-        file = 'S00%sR0%s.edf' % (subject, task)
-    if subject > 9 and subject < 100:
-        file = 'S0%sR0%s.edf' % (subject, task)
-    if subject > 99:
-        file = 'S%sR0%s.edf' % (subject, task)
-    return file
