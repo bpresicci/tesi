ce456609a2d5829f473ab529c1a2101de2e03b67
bpresicci <80779795+bpresicci@users.noreply.github.com>
2021-04-05 20:04:42 +0200

    Update filter_ScorEpochs.py

--- filter_ScorEpochs.py
+++ filter_ScorEpochs.py
@@ -6,16 +6,18 @@ def filter_ScorEpochs(dataset, labels, scores_user_specific, percentage):
     new_scores = []
     user = 0
     idx_ep = 0
-    scores_mean = np.mean(scores_user_specific[user])
     for epoch in range(len(dataset)):
-        if scores_user_specific[user][idx_ep] >= (scores_mean * percentage):
-            new_dataset = new_dataset + [dataset[epoch]]
-            new_labels = new_labels + [labels[epoch]]
-            new_scores = new_scores + [scores_user_specific[user]]
-        idx_ep += 1
-        if idx_ep == len(scores_user_specific[0]):
-            idx_ep = 0
-            user += 1
+      scores_mean = np.mean(scores_user_specific[user])
+      if scores_user_specific[user][idx_ep] >= (scores_mean * percentage):
+          new_dataset = new_dataset + [dataset[epoch]]
+          new_labels = new_labels + [labels[epoch]]
+          new_scores = new_scores + [scores_user_specific[user]]
+      idx_ep += 1
+      if idx_ep == len(scores_user_specific[0]):
+          idx_ep = 0
+          user += 1
+          if user < len(scores_user_specific):
+            scores_mean = np.mean(scores_user_specific[user])
     new_dataset = np.array(new_dataset)
     new_labels = np.array(new_labels)
     new_scores = np.array(new_scores)
