8cd98bf0f6a03027d5be6e046c13499efbec8595
bpresicci <80779795+bpresicci@users.noreply.github.com>
2021-04-07 15:02:14 +0200

    Add files via upload

--- filter_ScorEpochs.py
+++ filter_ScorEpochs.py
@@ -1,24 +1,27 @@
 import numpy as np
 
-def filter_ScorEpochs(dataset, labels, scores_user_specific, percentage):
-    new_dataset = []
-    new_labels = []
-    new_scores = []
-    user = 0
-    idx_ep = 0
-    for epoch in range(len(dataset)):
-      scores_mean = np.mean(scores_user_specific[user])
-      if scores_user_specific[user][idx_ep] >= (scores_mean * percentage):
+def filter_ScorEpochs(dataset, labels, scores, idx, percentage, tot_subjects, tot_conditions, subject_start, subject_end):
+  new_dataset = []
+  new_labels = []
+  new_scores = []
+  for condition in range(tot_conditions):
+    for subject in range(subject_start, subject_end):
+      if subject  not in [88, 92, 100]:
+        if condition == 0:
+          id_des = subject
+        else:
+          id_des = subject + tot_subjects
+      bool_id = idx == id_des
+      idx_ep = zip(*np.where(idx == id_des))
+      scores_mean = np.mean(scores[bool_id])
+      for epoch in idx_ep:
+        if scores[epoch] >= (scores_mean * percentage):
           new_dataset = new_dataset + [dataset[epoch]]
           new_labels = new_labels + [labels[epoch]]
-          new_scores = new_scores + [scores_user_specific[user]]
-      idx_ep += 1
-      if idx_ep == len(scores_user_specific[0]):
-          idx_ep = 0
-          user += 1
-          if user < len(scores_user_specific):
-            scores_mean = np.mean(scores_user_specific[user])
-    new_dataset = np.array(new_dataset)
-    new_labels = np.array(new_labels)
-    new_scores = np.array(new_scores)
-    return new_dataset, new_labels, new_scores
+          new_scores = new_scores + [scores[epoch]]
+    else:
+      continue
+  new_dataset = np.array(new_dataset)
+  new_labels = np.array(new_labels)
+  new_scores = np.array(new_scores)
+  return new_dataset, new_labels, new_scores
