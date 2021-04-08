1d1c08e182b6cc97939e7faaa64826ffa56e7253
bpresicci <80779795+bpresicci@users.noreply.github.com>
2021-04-08 14:05:06 +0200

    Add files via upload

--- filter_ScorEpochs.py
+++ filter_ScorEpochs.py
@@ -53,3 +53,30 @@ def filter_ScorEpochs_2(dataset, labels, scores, idx, percentage, tot_conditions
   new_labels = np.array(new_labels)
   new_scores = np.array(new_scores)
   return new_dataset, new_labels, new_scores
+
+def filter_ScorEpochs_3(dataset, labels, scores, idx, percentage, tot_conditions, subject_start, subject_end):
+  new_dataset = []
+  new_labels = []
+  new_scores = []
+  for condition in range(tot_conditions):
+    for subject in range(subject_start, subject_end):
+      if subject  not in [88, 92, 100]:
+        if condition == 0:
+          id_des = subject
+        else:
+          id_des = -subject
+        bool_id = idx == id_des
+        new_tot_epoch_per_user = int(percentage * len(idx[bool_id]))
+        subject_ep_idx = list(zip(*np.where(idx == id_des))) #indici delle epoche dell'utente specifico
+        position_random = np.arange(len(idx[bool_id]))
+        position_random = np.random.shuffle(position_random)
+        for i in range(new_tot_epoch_per_user):
+          new_dataset = new_dataset + [dataset[subject_ep_idx[position_random[i]]]]#idx_ep[idx_best[epoch]] è l'indice dell'epoca seguendo l'ordine dei punteggi migliori per ogni utent
+          new_labels = new_labels + [labels[subject_ep_idx[position_random[i]]]]
+          new_scores = new_scores + [scores[subject_ep_idx[position_random[i]]]]
+      else:
+        continue
+  new_dataset = np.array(new_dataset)
+  new_labels = np.array(new_labels)
+  new_scores = np.array(new_scores)
+  return new_dataset, new_labels, new_scores
