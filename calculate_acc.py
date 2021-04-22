681493bd1f7c20ac08c3ed02c7f24ba743622a68
bpresicci <80779795+bpresicci@users.noreply.github.com>
2021-04-22 13:11:56 +0200

    Add files via upload

--- calculate_acc.py
+++ calculate_acc.py
@@ -1,6 +1,10 @@
 import numpy as np
 
-def accuracy(accuracies):
+def accuracy_axis_1(accuracies):
     mean = np.mean(accuracies, 1)
     var = np.var(accuracies, 1)
+    return mean, var
+def accuracy_axis_2(accuracies):
+    mean = np.mean(accuracies, 2)
+    var = np.var(accuracies, 2)
     return mean, var
