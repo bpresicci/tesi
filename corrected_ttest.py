813a544914b3bb2cfeabb41efba84e7d44d6d589
bpresicci <80779795+bpresicci@users.noreply.github.com>
2021-04-26 12:35:17 +0200

    Add files via upload

--- corrected_ttest.py
+++ corrected_ttest.py
@@ -28,3 +28,19 @@ def corrected_dependent_ttest(data1, data2, n_training_folds, n_test_folds, alph
     p = (1.0 - t.cdf(abs(t_stat), df)) * 2.0 #formula comune anche su google
     # return everything
     return t_stat, df, cv, p
+
+def corrected_dependent_ttest_kfold(data1, data2, k, alpha): #formule Kuncheva
+    n = len(data1) #n è il numero di campioni
+    differences = [(data1[i]-data2[i]) for i in range(n)] #xij = aij − bij for fold i and run j
+    sd = stdev(differences)
+    divisor = 1 / n * sum(differences) #d
+    denominator = sqrt((2 * k - 1) / (k * (k - 1))) * sd
+    t_stat = divisor / denominator
+    # degrees of freedom
+    df = n - 1
+    #calculate the critical value
+    cv = t.ppf(1.0 - alpha, df)
+    # calculate the p-value
+    p = 2 * t.cdf(-(abs(divisor)/denominator), df)
+    # return everything
+    return t_stat, df, cv, p
