acd7ea5df6337bb952f2651fa41c9957e289c108
bpresicci <80779795+bpresicci@users.noreply.github.com>
2021-04-26 19:01:31 +0200

    Add files via upload

--- statistical_significance.py
+++ statistical_significance.py
@@ -97,23 +97,25 @@ def hold_out_nofiltri(names, classifiers, X_A, y_A, X_B, y_B):
 
 def applica_filtri(names, classifiers, kf, n_random, percentage, conditions, subject_start, subject_end, X_all, y_all, scores_all, id):
     for user_specific in [False, True]:
-        scores_togli_random_kfold = np.zeros((n_random, len(names), kf.get_n_splits()))
         if user_specific == False:
             scores_togli_n_ep_peggiori_kfold = filter_ScorEpochs(X_all, y_all, scores_all, id, percentage, filter_ScorEpochs_togli_n_ep_peggiori, kf, classifiers, user_specific, conditions, subject_start, subject_end)
             scores_togli_n_ep_migliori_kfold = filter_ScorEpochs(X_all, y_all, scores_all, id, percentage, filter_ScorEpochs_togli_n_ep_migliori, kf, classifiers, user_specific, conditions, subject_start, subject_end)
-            accuracy_togli_n_ep_peggiori_kfold_mean, accuracy_togli_n_ep_peggiori_kfold_var = accuracy_axis_1(scores_togli_n_ep_peggiori_kfold)
-            accuracy_togli_n_ep_migliori_kfold_mean, accuracy_togli_n_ep_migliori_kfold_var = accuracy_axis_1(scores_togli_n_ep_migliori_kfold)
-            for i in range(n_random):
-                scores_togli_random_kfold[i] = filter_ScorEpochs(X_all, y_all, scores_all, id, percentage, filter_ScorEpochs_togli_random, kf, classifiers, user_specific, conditions, subject_start, subject_end)
-            accuracy_togli_random_kfold_mean, accuracy_togli_random_kfold_var = accuracy_axis_2(scores_togli_random_kfold)
         else:
-            scores_togli_random_kfold_us = []
             scores_togli_n_ep_peggiori_kfold_us = filter_ScorEpochs(X_all, y_all, scores_all, id, percentage, filter_ScorEpochs_togli_n_ep_peggiori, kf, classifiers, user_specific, conditions, subject_start, subject_end)
             scores_togli_n_ep_migliori_kfold_us = filter_ScorEpochs(X_all, y_all, scores_all, id, percentage, filter_ScorEpochs_togli_n_ep_migliori, kf, classifiers, user_specific, conditions, subject_start, subject_end)
-            accuracy_togli_n_ep_peggiori_kfold_mean_us, accuracy_togli_n_ep_peggiori_kfold_var_us = accuracy_axis_1(scores_togli_n_ep_peggiori_kfold_us)
-            accuracy_togli_n_ep_migliori_kfold_mean_us, accuracy_togli_n_ep_migliori_kfold_var_us = accuracy_axis_1(scores_togli_n_ep_migliori_kfold_us)
-            for i in range(n_random):
-                scores_togli_random_kfold_us = scores_togli_random_kfold_us + [filter_ScorEpochs(X_all, y_all, scores_all, id, percentage, filter_ScorEpochs_togli_random, kf, classifiers, user_specific, conditions, subject_start, subject_end)]
-            scores_togli_random_kfold_us = np.array(scores_togli_random_kfold_us)
-            accuracy_togli_random_kfold_mean_us, accuracy_togli_random_kfold_var_us = accuracy_axis_2(scores_togli_random_kfold_us)
-    return scores_togli_n_ep_peggiori_kfold, accuracy_togli_n_ep_peggiori_kfold_mean, accuracy_togli_n_ep_peggiori_kfold_var, scores_togli_n_ep_migliori_kfold, accuracy_togli_n_ep_migliori_kfold_mean, accuracy_togli_n_ep_migliori_kfold_var, scores_togli_random_kfold, accuracy_togli_random_kfold_mean, accuracy_togli_random_kfold_var, scores_togli_n_ep_peggiori_kfold_us, accuracy_togli_n_ep_peggiori_kfold_mean_us, accuracy_togli_n_ep_peggiori_kfold_var_us, scores_togli_n_ep_migliori_kfold_us, accuracy_togli_n_ep_migliori_kfold_mean_us, accuracy_togli_n_ep_migliori_kfold_var_us, scores_togli_random_kfold_us, accuracy_togli_random_kfold_mean_us,accuracy_togli_random_kfold_var_us
+    return scores_togli_n_ep_peggiori_kfold, scores_togli_n_ep_migliori_kfold, scores_togli_n_ep_peggiori_kfold_us, scores_togli_n_ep_migliori_kfold_us
+
+def applica_random (names, classifiers, kf, n_random, percentage, conditions, subject_start, subject_end, X_all, y_all, scores_all, id):
+  for user_specific in [False, True]:
+    if user_specific == False:
+      scores_togli_random_kfold = []
+      for i in range(n_random):
+        scores_togli_random_kfold = scores_togli_random_kfold + [filter_ScorEpochs(X_all, y_all, scores_all, id, percentage, filter_ScorEpochs_togli_random, kf, classifiers, user_specific, conditions, subject_start, subject_end)]
+      scores_togli_random_kfold = np.array(scores_togli_random_kfold)
+    else:
+      scores_togli_random_kfold_us = []
+      for i in range(n_random):
+        scores_togli_random_kfold_us = scores_togli_random_kfold_us + [filter_ScorEpochs(X_all, y_all, scores_all, id, percentage, filter_ScorEpochs_togli_random, kf, classifiers, user_specific, conditions, subject_start, subject_end)]
+      scores_togli_random_kfold_us = np.array(scores_togli_random_kfold_us)
+  return scores_togli_random_kfold, scores_togli_random_kfold_us
+
