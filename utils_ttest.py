00ec35d15110b0fa4613b16ccee9077f1245f284
bpresicci <80779795+bpresicci@users.noreply.github.com>
2021-06-20 12:33:55 +0200

    Update utils_ttest.py

--- utils_ttest.py
+++ utils_ttest.py
@@ -38,20 +38,32 @@ def crea_liste(names):
     p_value3_us = np.zeros(len(names))
     return t_value1, t_value2, t_value3, p_value1, p_value2, p_value3, t_value1_us, t_value2_us, t_value3_us, p_value1_us, p_value2_us, p_value3_us
 
-def crea_KNN(perc, names, kf, n_random):
-    KNN_p_scores = np.zeros((len(perc), len(names), kf.get_n_splits()))
-    KNN_m_scores = np.zeros((len(perc), len(names), kf.get_n_splits()))
+def create_KNN(perc, names, kf, n_random):
+    """
+    It creates matrices to store the accuracies obtained in the different tests.
+     
+    INPUT:
+        perc: array of percentages used to remove the epochs from the initial dataset
+        names: list of the names of the classifiers used
+        kf: object created to perform the k-folds cross-validation
+        n_random: integer, it defines how many tests will be run to estimate the performance when removing random epochs
+        
+    OUTPUT:
+       Matrices meant to store the accuracies obtained in the different tests:
+        - "w" stands for "worst", the matrix will contain the accuracies obtained from the datasets created by removing the worst epochs.
+        - "b" stands for "best", the matrix will contain the accuracies obtained from the datasets created by removing the best epochs.
+        - "r" stands for "random", the matrix will contain the accuracies obtained from the datasets created by removing epochs randomly.
+              They have different dimensions because they include all 10 tests, not only one unlike the other cases.
+        - "ucs" stands for "user and condition specific", these matrices are meant to be used for that approach.
+    """
+    KNN_w_scores = np.zeros((len(perc), len(names), kf.get_n_splits()))
+    KNN_b_scores = np.zeros((len(perc), len(names), kf.get_n_splits()))
     KNN_r_scores_all = np.zeros((len(perc), n_random, len(names), kf.get_n_splits()))
-    KNN_us_p_scores = np.zeros((len(perc), len(names), kf.get_n_splits()))
-    KNN_us_m_scores = np.zeros((len(perc), len(names), kf.get_n_splits()))
-    KNN_us_r_scores_all = np.zeros((len(perc), n_random, len(names), kf.get_n_splits()))
-    KNN_p = np.zeros((len(perc), len(names)))
-    KNN_m = np.zeros((len(perc), len(names)))
-    KNN_r = np.zeros((len(perc), n_random, len(names)))
-    KNN_us_p = np.zeros((len(perc), len(names)))
-    KNN_us_m = np.zeros((len(perc), len(names)))
-    KNN_us_r = np.zeros((len(perc), n_random, len(names)))
-    return KNN_p_scores, KNN_m_scores, KNN_r_scores_all, KNN_us_p_scores, KNN_us_m_scores, KNN_us_r_scores_all, KNN_p, KNN_m, KNN_r, KNN_us_p, KNN_us_m, KNN_us_r
+    KNN_ucs_w_scores = np.zeros((len(perc), len(names), kf.get_n_splits()))
+    KNN_ucs_b_scores = np.zeros((len(perc), len(names), kf.get_n_splits()))
+    KNN_ucs_r_scores_all = np.zeros((len(perc), n_random, len(names), kf.get_n_splits()))
+    
+    return KNN_w_scores, KNN_b_scores, KNN_r_scores_all, KNN_ucs_w_scores, KNN_ucs_b_scores, KNN_ucs_r_scores_all
 
 def do_corrected_ttest(error_p, error_m, error_r, kf, alpha):
     t_value1, df, cv, p_value1 = corrected_dependent_ttest_kfold(error_p, error_r, kf.get_n_splits(), alpha)
